cddb83647615021c378014563d8af893edc87ac2
jamalshaik <jamaldevops.2020@gmail.com>
2020-03-09 23:14:34 +0530

    Pushing the changes

--- 02-Variable/02.list/list.tf
+++ 02-Variable/02.list/list.tf
@@ -1,11 +1,15 @@
-variable "CLASSES" {
-  default = ["AWS","AZURE"]
+variable      "CLASSES" {
+  default     = ["AWS","AZURE","GCP"]
 }
 
-output "First_Class"{
-  value = var.CLASSES[0]
+output        "First_Class"{
+  value       = var.CLASSES[0]
 }
 
-output "Second_class" {
-  value = var.CLASSES[1]
+output        "Second_class" {
+  value       = var.CLASSES[1]
+}
+
+output        "Thrid_class"{
+  value       = var.CLASSES[2]
 }
